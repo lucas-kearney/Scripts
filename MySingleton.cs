3cf417c0d79480fa4c756a9d68601a59b32692ef
lucas-kearney <lucas.kearney@cuw.edu>
2024-02-13 19:58:08 -0600

    between scenes

---
+++ MySingleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySingleton
+{
+   public GameObject northExit;
+    public GameObject southExit;
+    public GameObject eastExit;
+    public GameObject westExit;
+
+    public bool northActivated = false;
+    public bool southActivated = false;
+    public bool eastActivated = false;
+    public bool westActivated = false;
+}
--- Player.cs
+++ Player.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+   private Rigidbody rb;
+   private float movementX;
+    private float movementY;
+    public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent <Rigidbody>();
+        print("You are now in room one");
+    }
+
+
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //EditorSceneManager.LoadScene("Scene2");
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnMove (InputValue movementValue)
+   {
+    Vector2 movementVector = movementValue.Get<Vector2>(); 
+    this.movementX = movementVector.x; 
+    this.movementY = movementVector.y;
+   }
+
+    private void FixedUpdate() 
+   {
+    Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+    rb.AddForce(movement * speed);
+   }
+    
+   
 }
